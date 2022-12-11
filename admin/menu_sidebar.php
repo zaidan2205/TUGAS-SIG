176a1e888d0147e533387303c70314707f833fad
Zaidan Nabil Diwangkara <72377693+zaidan2205@users.noreply.github.com>
2022-12-11 17:53:11 +0700

    Add files via upload

---
+++ admin/menu_sidebar.php
@@ -0,0 +1,50 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon ">
+            <img class="rounded-circle" src="img/jateng.png" width="45px" height="45px" alt="...">
+        </div>
+        <div class="sidebar-brand-text mx-3">SIG Wisata Jawa Tengah</div>
+
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="tampil_data.php">
+            <i class="fas fa-fw fa-user-friends"></i>
+            <span>Data Wisata</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tambah_data.php">
+            <i class="fas fa-fw fa-user-friends"></i>
+            <span>Tambah Data</span></a>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+
+</ul>
+<!-- End of Sidebar -->
